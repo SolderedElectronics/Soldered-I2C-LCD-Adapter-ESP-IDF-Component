43ee6644385353f41f3641ad4309f4c09d663cf2
Fran Fodor <49796718+franFodor@users.noreply.github.com>
2026-08-25 12:32:07 +0200

    Initial commit

---
+++ src/soldered_esp_idf_component_template.c
@@ -0,0 +1,13 @@
+/**
+ * @file soldered_esp_idf_component_template.c
+ * @brief Implementation for the soldered-esp-idf-component-template component
+ * @author Soldered Electronics
+ */
+
+#include <stdio.h>
+#include "soldered_esp_idf_component_template.h"
+
+void func(void)
+{
+    printf("Hello from soldered-esp-idf-component-template!\n");
+}
